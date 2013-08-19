af91052d83b06de54a37082a245745f4e8ff4771
Daniele Pizzolli <daniele@ahref.eu>
2013-08-19 14:16:28 +0200

    Auto add http:// on the save of website
    Fix #198

--- ProfileManager/blueprints/user/static/js/custom.js
+++ ProfileManager/blueprints/user/static/js/custom.js
@@ -1,4 +1,20 @@
 $(function($) {
+
+    // helper function to fix the url prepending http
+    function fixURL(url){
+	var r = url.match(/^(https?:\/\/)?(.*)$/);
+	return ((r[1] ? r[1] : 'http://') + r[2]);
+    }
+
     // use pageslide for login
     $(".login").pageslide({ direction: "left", modal: false, href: '#login-form' });
+
+
+    // check for proper http in the url
+    $("form").submit(function() {
+        if (! $("#website").hasClass('hint')) {
+	    $("#website").val(fixURL($("#website").val()));
+	}
+    });
+
 });
